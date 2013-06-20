185a3d66cd1cc663fe416a5c87caaed41b4b7798
Andrew Curry <andrew.curry@nextcentury.com>
2013-06-19 21:58:10 -0400

    some very minor app work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,9 @@
 
 angular.module('fortisApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.fitnessTypes = [
+      'crossfit',
+      'running',
+      'weightlifting'
     ];
   });
